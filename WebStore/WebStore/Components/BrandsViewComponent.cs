ff908514ee430615e99e1be6b376e5fb82a9056f
Admin <deepar@yandex.ru>
2019-04-09 21:11:37 +0300

    lesson 04

---
+++ WebStore/WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+	public class BrandsViewComponent : ViewComponent
+	{
+		private readonly IProductData _ProductData;
+
+		public BrandsViewComponent(IProductData ProductData)
+		{
+			_ProductData = ProductData;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var brands = GetBrands();
+			return View(brands);
+		}
+
+		private IEnumerable<BrandViewModel> GetBrands()
+		{
+			return _ProductData.GetBrands().Select(c => new BrandViewModel()
+			{
+				Id = c.Id,
+				Name = c.Name,
+				Order = c.Order,
+			});
+		}
+	}
+}
---
+++ WebStore/WebStore/Infrastructure/Implementations/InMemoryProductData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Implementations
+{
+	public class InMemoryProductData : IProductData
+	{
+		public IEnumerable<Section> GetSections() => TestData.Sections;
+
+		public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+		public IEnumerable<Product> GetProducts(ProductFilter Filter)
+		{
+			var products = (IEnumerable<Product>)TestData.Products;
+			if (Filter is null) return products;
+			if (Filter.BrandId != null)
+				products = products.Where(c => c.BrandId == Filter.BrandId);
+			if (Filter.SectionId != null)
+				products = products.Where(c => c.SectionId == Filter.SectionId);
+			return products;
+		}
+	}
+}

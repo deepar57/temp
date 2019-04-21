05d3568c17da82b97e0f06c665683fa519a7aa8d
deepar57 <47324708+deepar57@users.noreply.github.com>
2019-04-21 12:51:09 +0300

    07

--- WebStore/WebStore.Domain/Entities/User.cs
+++ WebStore/WebStore.Domain/Entities/User.cs
@@ -7,5 +7,10 @@ namespace WebStore.Domain.Entities
 {
 	public class User : IdentityUser
 	{
+		public const string AdminUserName = "Admin";
+		public const string DefaultAdminPassword = "AdminPassword";
+
+		public const string RoleAdmin = "Administrator";
+		public const string RoleUser = "User";
 	}
 }
